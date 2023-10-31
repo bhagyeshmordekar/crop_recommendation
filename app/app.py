f96c31b971a2a03f5840d814147e498dd674c8eb
iron man <ironman@gmail.com>
2023-10-31 22:43:41 +0530

    first commit

---
+++ app/app.py
@@ -0,0 +1,130 @@
+
+
+from flask import Flask, render_template, request, Markup
+import numpy as np
+import pandas as pd
+
+import requests
+import config
+import pickle
+import io
+import torch
+from torchvision import transforms
+from PIL import Image
+
+import sys
+
+crop_recommendation_model_path = 'models/RandomForest0.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+
+
+# =========================================================================================
+
+# Custom functions for calculations
+
+
+def weather_fetch(city_name):
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+
+
+
+# ===============================================================================================
+# ------------------------------------ FLASK APP -------------------------------------------------
+
+
+app = Flask(__name__)
+
+# render home page
+
+
+@ app.route('/')
+def home():
+    title = ''
+    return render_template('index.html', title=title)
+
+# render crop recommendation form page
+
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+# render fertilizer recommendation form page
+
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    pass
+
+
+
+
+# ===============================================================================================
+
+# RENDER PREDICTION PAGES
+
+# render crop recommendation result page
+
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = 'Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        temperature1 = int(request.form['temperature'])
+        humidity1 = int(request.form['humidity'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+         temperature, humidity = weather_fetch(city)
+         data = np.array([[N, P, K, temperature1, humidity1, ph, rainfall]])
+         my_prediction = crop_recommendation_model.predict(data)
+         final_prediction = my_prediction[0]
+
+         return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+         data = np.array([[N, P, K, temperature1, humidity1, ph, rainfall]])
+         my_prediction = crop_recommendation_model.predict(data)
+         final_prediction = my_prediction[0]
+
+         return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+            # return render_template('try_again.html', title=title)
+
+
+
+
+@app.route('/disease-predict', methods=['GET', 'POST'])
+def disease_prediction():
+    pass
+
+
+# ===============================================================================================
+if __name__ == '__main__':
+    app.run(debug=False)
